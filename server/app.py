3052de99b6cc38f07b5625ba6119f02f3d180c7f
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-02 17:32:48 +0300

    ain

--- server/app.py
+++ server/app.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python3
-import os
-#from werkzeug import Response,Request
-from flask import Flask,request,current_app,g,make_response
+# #!/usr/bin/env python3
+# import os
+# #from werkzeug import Response,Request
+# from flask import Flask,request,current_app,g,make_response
 
-app = Flask(__name__)
-@app.before_request
-def app_path():
-    g.path=os.path.abspath((os.getcwd()))
+# app = Flask(__name__)
+# @app.before_request
+# def app_path():
+#     g.path=os.path.abspath((os.getcwd()))
     
-@app.route("/")
+# @app.route("/")
+# def index():
+#     host=request.headers.get("host")
+#     appname=current_app.name
+#     response_body=f'''
+#       <h1>The host for this page is {host}
+#       <h1>The name for this application is {appname}
+#       <h1>The  path of this application on the user's device is {g.path}</h1>
+
+# '''
+#     status_code=200
+#     headers={}
+#     return make_response(response_body,status_code,headers)
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+from flask import Flask,current_app,request
+app=Flask(__name__)
+@app.route
 def index():
     host=request.headers.get("host")
-    appname=current_app.name
-    response_body=f'''
-      <h1>The host for this page is {host}
-      <h1>The name for this application is {appname}
-      <h1>The  path of this application on the user's device is {g.path}</h1>
-
+    app_name=current_app.name
+    return f'''<h1>The host of this page is {host}</h1>
+               <h1>The name for this application is {app_name}
 '''
-    status_code=200
-    headers={}
-    return make_response(response_body,status_code,headers)
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+if __name__=="__main__":
+    app.run(port=5555,debug=True)
